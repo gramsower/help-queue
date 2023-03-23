baf14aee9681fe3ea6e2ed1ac9a427c30492b0fd
gramsower <110431307+gramsower@users.noreply.github.com>
2023-03-22 18:45:59 -0700

    squashed :bug:

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,11 @@
 import React from "react";
-import helpDeskImage from "./../img/helpdesk.jpg"
+import helpDeskImage from "./../img/helpdesksmall.jpg"
 
 function Header() {
   return (
     <React.Fragment>
     <h1>Help Queue</h1>
-    {/* <img src={helpDeskImage} alt="A neat desk" /> */}
+    <img src={helpDeskImage} alt="A neat desk" />
     </React.Fragment>
   );
 }
